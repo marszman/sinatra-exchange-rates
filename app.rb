d5d0b5b106683a8f6346480b24720edb2b6a5da8
marszman <136658905+marszman@users.noreply.github.com>
2023-07-17 21:39:25 +0000

    exchange rates - APIs complete

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "http"
+require "json"
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  symbols_url = "https://api.exchangerate.host/symbols"
+  raw_symbols_data = HTTP.get(symbols_url)
+  parsed_symbols_data = JSON.parse(raw_symbols_data)
+  full_symbols_hash = parsed_symbols_data.fetch("symbols")
+  symbols_hash = full_symbols_hash.keys
+end
+
+#get("/:currency_from/:currency_to") do
+get("/USD/INR") do
+  currency_from = "USD"
+  currency_to = "INR"
+  conversionrate_url = "https://api.exchangerate.host/convert?from=#{currency_from}&to=#{currency_to}"
+  raw_conversionrate_data = HTTP.get(conversionrate_url)
+  parsed_conversionrate_data = JSON.parse(raw_conversionrate_data)
+  test = parsed_conversionrate_data.fetch("result")
+  pp test.to_s
+  # symbols_hash = full_symbols_hash.keys
 end
