3d504a5d18b3548749883b19bafa077d6f033333
marszman <136658905+marszman@users.noreply.github.com>
2023-07-18 01:06:02 +0000

    exchange rate - first page complete

--- app.rb
+++ app.rb
@@ -9,8 +9,13 @@ get("/") do
   raw_symbols_data = HTTP.get(symbols_url)
   parsed_symbols_data = JSON.parse(raw_symbols_data)
   full_symbols_hash = parsed_symbols_data.fetch("symbols")
-  symbols_hash = full_symbols_hash.keys
+  @symbols_array = full_symbols_hash.keys
+  erb(:first_page)
 end
+
+#get("/:currency_from")
+  #erb(:second_page)
+#end
 
 #get("/:currency_from/:currency_to") do
 get("/USD/INR") do
@@ -19,7 +24,6 @@ get("/USD/INR") do
   conversionrate_url = "https://api.exchangerate.host/convert?from=#{currency_from}&to=#{currency_to}"
   raw_conversionrate_data = HTTP.get(conversionrate_url)
   parsed_conversionrate_data = JSON.parse(raw_conversionrate_data)
-  test = parsed_conversionrate_data.fetch("result")
-  pp test.to_s
-  # symbols_hash = full_symbols_hash.keys
+  coversionrate = parsed_conversionrate_data.fetch("result")
+  conversionrate_string = conversionrate.to_s
 end
