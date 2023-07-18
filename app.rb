900ed845e68339466a4b096b140c366ec2d93096
marszman <136658905+marszman@users.noreply.github.com>
2023-07-18 01:37:50 +0000

    conversion rate - complete

--- app.rb
+++ app.rb
@@ -13,17 +13,23 @@ get("/") do
   erb(:first_page)
 end
 
-#get("/:currency_from")
-  #erb(:second_page)
-#end
+get("/:currency_f") do
+  symbols_url = "https://api.exchangerate.host/symbols"
+  raw_symbols_data = HTTP.get(symbols_url)
+  parsed_symbols_data = JSON.parse(raw_symbols_data)
+  full_symbols_hash = parsed_symbols_data.fetch("symbols")
+  @symbols_array = full_symbols_hash.keys
+  @currency_from = params.fetch("currency_f")
+  erb(:second_page)
+end
 
-#get("/:currency_from/:currency_to") do
-get("/USD/INR") do
-  currency_from = "USD"
-  currency_to = "INR"
-  conversionrate_url = "https://api.exchangerate.host/convert?from=#{currency_from}&to=#{currency_to}"
+get("/:currency_f/:currency_t") do
+  @currency_from = params.fetch("currency_f")
+  @currency_to = params.fetch("currency_t")
+  conversionrate_url = "https://api.exchangerate.host/convert?from=#{@currency_from}&to=#{@currency_to}"
   raw_conversionrate_data = HTTP.get(conversionrate_url)
   parsed_conversionrate_data = JSON.parse(raw_conversionrate_data)
-  coversionrate = parsed_conversionrate_data.fetch("result")
-  conversionrate_string = conversionrate.to_s
+  conversionrate = parsed_conversionrate_data.fetch("result")
+  @conversionrate_string = conversionrate.to_s
+  erb(:conversion)
 end
